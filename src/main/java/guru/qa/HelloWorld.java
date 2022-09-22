0a4502b678ca455b9240a9b7ebc5582dd4cb711c
Evharisto <sv.lisitskaya@mail.ru>
2022-09-22 18:28:22 +0300

    commit

---
+++ src/main/java/guru/qa/HelloWorld.java
@@ -0,0 +1,33 @@
+package guru.qa;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        String stingThatShouldBeDisplayedInConsole = "Hello, World!";
+        boolean flag = true;
+        boolean flag0 = false;
+        byte aByte; //8 бит
+        short aShort; //16 бит
+        int aInt; //32 бит
+        long aLong; //64 бит
+        char aChar = '1';
+        System.out.println(stingThatShouldBeDisplayedInConsole);
+
+        //Операторы математические: +, -, *, /, % (остаток от деления)
+        //Операторы сравнения: >, <, >=, <=, == (равно), != (не равно)
+        //Логические операторы: && (и), || (или), ! (отрицание)
+
+        int a = 10;
+        int b = 15;
+
+        System.out.println(a + b);
+
+        if (stingThatShouldBeDisplayedInConsole != null && stingThatShouldBeDisplayedInConsole.length() == 3) {
+            System.out.println("Строка из 3 символов : " + stingThatShouldBeDisplayedInConsole);
+        } else if (stingThatShouldBeDisplayedInConsole == null) {
+            System.out.println("Строка null : " + stingThatShouldBeDisplayedInConsole);
+        } else {
+            System.out.println("Строка не из 3 символов : " + stingThatShouldBeDisplayedInConsole);
+        }
+    }
+}
